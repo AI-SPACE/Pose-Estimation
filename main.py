1054a01f9f13e01d73b495044cfc8d619e08ee6d
Gauri Bhardwaj <gauribhardwaj1211@gmail.com>
2022-03-04 00:05:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from pickle import TRUE
 from time import ctime
 import cv2
-import mediapipe
+import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture('PoseVideos/1.mp4')
-pTime=0,
+
+mpDraw =mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+
+cap = cv2.VideoCapture('PoseVideos/2.mp4')
+pTime=0
 while TRUE:
     success, img = cap.read()
-         
-    
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results =pose.process(imgRGB)
+    print(results.pose_landmarks)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks,mpPose.POSE_CONNECTIONS)
+
     cTime = time.time()
     fps= 1/(cTime - pTime)
     pTime = cTime
